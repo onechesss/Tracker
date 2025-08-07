3544095d527b67c63e1869e798d407a997cc407d
onechesss <moskwin.vanya2017@yandex.ru>
2025-08-07 04:28:37 +0300

    добавлен экран онбординга

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -18,7 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = TabBarController()
+#warning("TODO: доделать выбор экрана (онбординг или трекеры) в зависимости от того, первый ли это запуск приложения")
+        window?.rootViewController = OnboardingPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
         window?.makeKeyAndVisible()
     }
 
